2bfbba596af0d5568c46e14f40132992ac09e063
Sarah Davis <sdavis2@nic.edu>
2026-04-22 14:13:17 -0700

    can't remember what I changed

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-int add(int a, int b);
+int add(int &a, int &b);
 
 int main()
 {
     cout << "Greetings.....\n";
     cout << "Hello World\n";
 
+    int x = 5;
+    int y = 3;
+    add(x, y);
     return 0;
 }
 
-int add(int a, int b)
+int add(int &a, int &b)
 {
-    return a + b;
+    a = 7;
+    int sum = a + b;
+    return sum;
 }
